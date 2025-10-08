6c95b1172bebc691c8eddea1b6fd946b3586915b
Tyler Brotherton <105938811+tylerbrotherton@users.noreply.github.com>
2025-10-08 16:42:52 -0500

    Update headerFileExample.h
    Added a guard function to the header file, to match the style and convention of c programming.

--- PreLab7/headerFileExample.h
+++ PreLab7/headerFileExample.h
@@ -1,3 +1,6 @@
+#ifndef HEADEREXAMPLE_H_ //function guard
+#define HEADEREXAMPLE_H_
+
 int reset_day(void);
 int record_purchase(int cents);
 int compute_points(int cents);
@@ -5,8 +8,17 @@ int get_total_points(void);
 int redeem_reward(void);
 
 int points;//global variable, unfortantely
+            // it would make a lot of logical
+            //sense to have a global variable
+            //but the instructor doesn't want 
+            //it
+
+            //the code will not compile without
+            //it so it's still here
 
 int bouns_points_day(int multiplier)
+
+#endif //HEADEREXAMPLE_H_
 
 //RESETS POINTS
 int reset_day(void){
@@ -16,16 +28,16 @@ int reset_day(void){
 //RETURNS 1 IF SUCCESSFUL, -1 IF NOT SUCCESSFUL(which means negitive cents value)
 //and 5 when it cents is 0 because thats undefined behavior
 int record_purchase(int cents){
-      if(cents<0)
+      if(cents < 0)
           return -1;
-      else{//else is unnessacry but gives helps stability/debuging help for mass deployment
+      else{ //else is unnessacry but gives helps stability/debuging help for mass deployment
           points+=cents
           return 1;
       }
       return 5;
 }
 int compute_points(int cents){
-      return (cents/100);
+      return (cents / 100);
 }
 int get_total_points(void){
       return points;
@@ -36,8 +48,8 @@ int get_total_points(void){
 //returns 5 for error
 //if points_needed is negitive, the function will hurt the firm, but can't be changed now.
 int redeem_reward(int points_needed){
-    if(points_needed<=points){
-          points-=points_needed;
+    if(points_needed <= points){
+          points-= points_needed;
           return 1;
     }
      else
@@ -46,7 +58,7 @@ int redeem_reward(int points_needed){
 }
 
 int bouns_points_day(int multiplier){
-    points*=multiplier;
+    points *= multiplier;
     return points;
 }
 
